3f3beb6614dec3c31ea563cb667af7649f32d54c
Jaysidh <jay.dumbali@gmail.com>
2020-05-02 21:44:00 +0530

    Updates

---
+++ precache-manifest.5cf83d72859b82010d89ee22dc963e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ae1bd883b130f20413cfb351bea5ed",
+    "url": "/Random-Avatar/index.html"
+  },
+  {
+    "revision": "94504e4bb6e6640c878e",
+    "url": "/Random-Avatar/static/css/main.af14bb73.chunk.css"
+  },
+  {
+    "revision": "4643b02188f688a4f16b",
+    "url": "/Random-Avatar/static/js/2.5c532459.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Avatar/static/js/2.5c532459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94504e4bb6e6640c878e",
+    "url": "/Random-Avatar/static/js/main.f03c955e.chunk.js"
+  },
+  {
+    "revision": "27b23974b0d6ab72d69e",
+    "url": "/Random-Avatar/static/js/runtime-main.18817e38.js"
+  }
+]);
